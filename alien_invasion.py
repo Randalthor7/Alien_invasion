3c74829563dc048d1c3dbbc037efab6641c7fe57
Alistair L Jones <Spacesushi7@gmail.com>
2022-09-08 15:39:36 +0100

    the ship is ready

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+from settings import settings
+from ship import Ship
 
 class AlienInvasion:
 
@@ -9,23 +11,27 @@ class AlienInvasion:
         """Initalize the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
 
-        #set the background colour.
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
-        def run_game(self):
-            """start the main loop for the game"""
-            while True:
-                #Watch for keyboard and mouse events
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
-                #Redraw the screen during each pass through the loop.
-                self.screen.fill(self.bg_color)
+    def run_game(self):
+        """start the main loop for the game"""
+        while True:
+            #Watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            #Make the most recently drawn screen visible
+            pygame.display.flip()
 
-                #Make the most recently drawn screen visible
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
