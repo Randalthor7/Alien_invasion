b06582e4335399d9af4bddfa845a1ce4ab378e9f
Alistair L Jones <Spacesushi7@gmail.com>
2022-09-09 15:47:44 +0100

    the ship moves and cannot go past the screen, refactored check events.

--- ship.py
+++ ship.py
@@ -1,10 +1,12 @@
 import pygame
 
+
 class Ship:
     """A class to manage the ship"""
     def __init__(self,ai_game):
         """ Initalize the ship and its starting postion."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -13,6 +15,25 @@ class Ship:
 
         #Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ships horizontal postion.
+        self.x = float(self.rect.x)
+
+        #Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ship's postion based on the movement flag."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #update rect object from self.x.
+        self.rect.x = self.x
+
 
     def blitme(self):
         """draw the ship at its current location."""
